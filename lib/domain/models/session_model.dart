6073e6ea53cd4ddaa364970e91f839499c5357d0
macbook <mirvoxidraxmonbekov@gmail.com>
2025-04-28 23:25:30 +0500

    add session model

---
+++ lib/domain/models/session_model.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: non_constant_identifier_names
+import 'package:timerflow/domain/models/table_model.dart';
+
+class SessionModel {
+  int? id;
+  int table_id;
+  int? order_id;
+  DateTime start_time;
+  DateTime? end_time;
+  TableModel? tableModel;
+
+  SessionModel({
+    this.id,
+    required this.table_id,
+     this.order_id,
+    required this.start_time,
+     this.end_time,
+    this.tableModel,
+  });
+
+  factory SessionModel.fromJson(Map<String, dynamic> json) {
+    return SessionModel(
+      id: json['id'],
+      table_id: json['table_id'],
+      order_id: json['order_id'],
+      start_time: DateTime.parse(json['start_time']),
+      end_time: DateTime.parse(json['end_time']),
+      tableModel: json['tables'] != null
+          ? TableModel.fromJson(json['tables'])
+          : null,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'table_id': table_id,
+      'order_id': order_id,
+      'start_time': start_time.toIso8601String(),
+      'end_time': end_time?.toIso8601String(),
+    };
+  }
+
+}
