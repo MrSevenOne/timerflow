0f6bb2ae68406d6741ea3f4b091f1cbe6c1a2a0d
macbook <mirvoxidraxmonbekov@gmail.com>
2025-04-29 22:55:09 +0500

    create widget for orders item

---
+++ lib/ presentation/widgets/order_widget/food_item.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:timerflow/domain/models/orders_model.dart';
+
+class FoodOrderItem extends StatelessWidget {
+  final OrderFoodModel order;
+
+  const FoodOrderItem({super.key, required this.order});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text('${order.foodModel?.name ?? 'Nomaʼlum'}'),
+        trailing: Text('Soni: ${order.quantity}'),
+        subtitle: Text('Narxi: ${order.foodModel?.price ?? 0} so\'m'),
+      ),
+    );
+  }
+}
