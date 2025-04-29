1756e5dde80cb3c29e1ad02f3ed153dfd8afb9ce
macbook <mirvoxidraxmonbekov@gmail.com>
2025-04-29 22:54:22 +0500

    edit date formatted '#,##0' from '#,###"

--- lib/utils/formatter/date_formatter.dart
+++ lib/utils/formatter/date_formatter.dart
@@ -1,8 +1,28 @@
 import 'package:intl/intl.dart';
 
 class DateFormatter {
-  static String format({required DateTime date}) {
+  static String formatWithMonth({required DateTime date}) {
     final formatter = DateFormat('HH:mm dd MMM');
     return formatter.format(date);
   }
+
+  static String formatTime({required DateTime time}) {
+    final formatter = DateFormat('HH:mm');
+    return formatter.format(time);
+  }
+
+ static String formatDuration(Duration duration) {
+  final int hours = duration.inHours;
+  final int minutes = duration.inMinutes.remainder(60);
+
+  if (hours > 0) {
+    return '${_twoDigits(hours)}:${_twoDigits(minutes)}';
+  } else {
+    return '${_twoDigits(minutes)} minut';
+  }
+}
+
+static String _twoDigits(int n) => n.toString().padLeft(2, '0');
+
+
 }
