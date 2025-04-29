5f908094cfde5497d5c73b31cdf3c88ed7a8307a
macbook <mirvoxidraxmonbekov@gmail.com>
2025-04-29 22:53:37 +0500

    formater void change price void

--- lib/utils/formatter/number_formatted.dart
+++ lib/utils/formatter/number_formatted.dart
@@ -1,8 +1,8 @@
 import 'package:intl/intl.dart';
 
 class NumberFormatter {
-  static String format(int number) {
-    final formatter = NumberFormat('#,##0', 'uz_UZ');
+  static String price(int number) {
+    final formatter = NumberFormat('#,###', 'uz_UZ');
     return formatter.format(number);
   }
 }
