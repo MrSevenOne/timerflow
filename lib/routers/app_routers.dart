cf088f98105822393b810a831223ed7a448f4978
macbook <mirvoxidraxmonbekov@gmail.com>
2025-04-29 22:52:51 +0500

    add OrderPage router

--- lib/routers/app_routers.dart
+++ lib/routers/app_routers.dart
@@ -1,32 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:timerflow/%20presentation/screens/bar_page.dart';
+import 'package:timerflow/%20presentation/screens/order_page.dart';
 import 'package:timerflow/%20presentation/screens/drink_page.dart';
 import 'package:timerflow/%20presentation/screens/foods_page.dart';
 import 'package:timerflow/%20presentation/screens/session_page.dart';
 import 'package:timerflow/%20presentation/screens/tables_page.dart';
 
-import 'package:timerflow/domain/models/session_model.dart'; // <-- model import qilish kerak
 
 class AppRoutes {
   static const String home = '/';
   static const String foods = '/FoodPage';
   static const String drinks = '/DrinkPage';
-  static const String bar = '/BarPage';
+  static const String order = '/OrderPage';
   static const String session = '/SessionPage';
 
   static Map<String, WidgetBuilder> routes = {
     home: (context) => const TablePage(),
     foods: (context) => const FoodPage(),
     drinks: (context) => const DrinkPage(),
-    bar: (context) => const BarPage(),
   };
 
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case session:
-        final args = settings.arguments as SessionModel; // <-- argumentsni olish
+        final args = settings.arguments as int; // <-- argumentsni olish
         return MaterialPageRoute(
-          builder: (context) => SessionPage(sessionModel: args),
+          builder: (context) => SessionPage(tableId: args),
+        );
+      case order:
+        final arg = settings.arguments as int;
+        return MaterialPageRoute(
+          builder: (context) => OrderPage(sessionId: arg),
         );
       default:
         return MaterialPageRoute(
